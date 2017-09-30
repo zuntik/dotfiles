016beb3c01081e30b8eb60f1162a9eda253f8f7f
thomas <thomasdpb@gmail.com>
2017-09-30 16:46:13 +0100

    forgot to remove gtk from copier

--- copier.sh
+++ copier.sh
@@ -8,9 +8,9 @@ BACKUPFOLDER=$(date +backup-%d-%m-%yat%I-%M-%S);
 mkdir $HOME/dotfiles/$BACKUPFOLDER;
 
 # files that go in home
-DIRS1=("bash" "aliases" "zsh" "X" "git" "systemd" "gtk-3.0" "compton" "yaourt")
+DIRS1=("bash" "aliases" "zsh" "X" "git" "systemd" "compton" "yaourt")
 # directories that go in ~/.config/
-DIRS2=("i3" "i3status" "dunst" "rofi" "rofi-pass" "gtk-3.0")
+DIRS2=("i3" "i3status" "dunst" "rofi" "rofi-pass")
 # directories that go in home
 DIRS3=("ssh" "vim" "gnupg")
 
