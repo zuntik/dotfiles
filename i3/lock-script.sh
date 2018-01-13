03031886c3b328023734a5c8615de047a37e9886
thomas <thomasdpb@gmail.com>
2018-01-13 01:38:15 +0000

    i3 lock is blurry screenshot

--- i3/lock-script.sh
+++ i3/lock-script.sh
@@ -7,8 +7,11 @@ image=/tmp/screen.png
 # get the screenshot
 scrot "$image"
 # add the blur
-convert "$image" -scale 10% -scale 1000% "$image"
+#convert "$image" -scale 10% -scale 1000% "$image"
+#convert "$image" -filter Gaussian -resize "250.00" -define "filter:sigma=.6" -resize "1366x768^" -gravity center -extent "1366x768^" "$image"
+#convert "$image" -filter Gaussian -resize "175.00" -define "filter:sigma=.6" -resize "1366x768^" -gravity center -extent "1366x768^" "$image"
+convert "$image" -filter Gaussian -resize "300.00" -define "filter:sigma=.9" -resize "1366x768^" -gravity center -extent "1366x768^" "$image"
 # lock the screen
-i3lock -i "$image"
+i3lock -e -i "$image"
 # cleanup image
 rm "$image"
