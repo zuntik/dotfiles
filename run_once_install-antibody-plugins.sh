e33651ea299e3e31c4b0ad45c1a55b0eaf17f8fa
Thomas Berry <thomasdpb@gmail.com>
2021-01-26 00:02:55 +0000

    shell scripts in legacybin

--- run_once_install-antibody-plugins.sh
+++ run_once_install-antibody-plugins.sh
@@ -1,3 +1,4 @@
+#!/bin/zsh
 if ! foobar_loc="$(type -p "antibody")" || [[ -z $foobar_loc ]]; then
 	antibody bundle < ${XDG_CONFIG_HOME:-$HOME/.config}/zsh/.zsh_plugins.txt > ${XDG_CONFIG_HOME:-$HOME/.config}/zsh/.zsh_plugins.sh 
 fi
